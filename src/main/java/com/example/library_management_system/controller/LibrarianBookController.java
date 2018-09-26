82426ca14930574abf1153a858c0be2535c86daf
majikaisteve123456 <411610827@qq.com>
2018-09-26 21:18:16 +0800

    增删、显示图书

---
+++ src/main/java/com/example/library_management_system/controller/LibrarianBookController.java
@@ -0,0 +1,42 @@
+package com.example.library_management_system.controller;
+
+import com.example.library_management_system.bean.Book;
+import com.example.library_management_system.service.LibrarianBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.example.library_management_system.bean.Bkunit;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
+import java.util.Set;
+
+@Controller
+public class LibrarianBookController {
+
+  @Autowired
+    private LibrarianBookService librarianBookService;
+
+  @RequestMapping(path={"/ManagingBook/deletebook"},method={RequestMethod.POST})
+    public boolean  deleteBook(Bkunit bkunit)
+  {
+      librarianBookService.deleteBook(bkunit);
+       return true;
+  }
+
+   @RequestMapping(path={"/ManagingBook/addbook"},method={RequestMethod.POST})
+    public boolean  addBook(Bkunit bkunit)
+   {
+       librarianBookService.addBook(bkunit);
+       return true;
+   }
+//返回页面booksinfo
+   @RequestMapping(path={"/ManagingBook/showbook"},method={RequestMethod.GET})
+    public String showBook(Model model,@RequestParam(value="start",defaultValue="0")int start,@RequestParam(value="size",defaultValue = "10")int size)throws Exception
+   {
+       Page<Bkunit> page=librarianBookService.showBook(start,size);
+       model.addAttribute("page",page);
+       return "booksinfo";
+   }
+}
