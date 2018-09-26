d4d365d9b4c6bbc8d03d2dbd89411a16604d476f
Captain <onepiece.captain97@gmail.com>
2018-09-26 21:28:17 +0800

    add UserFavoriteBook

--- src/main/java/com/example/library_management_system/service/ReaderFunctionService.java
+++ src/main/java/com/example/library_management_system/service/ReaderFunctionService.java
@@ -1,8 +1,12 @@
 package com.example.library_management_system.service;
 
+import com.example.library_management_system.bean.Book;
 import com.example.library_management_system.bean.User;
 import com.example.library_management_system.bean.UserBkunit;
+import com.example.library_management_system.bean.UserFavoriteBook;
+import com.example.library_management_system.dao.BookDAO;
 import com.example.library_management_system.dao.UserBkunitDAO;
+import com.example.library_management_system.dao.UserFavoriteBookDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -26,8 +30,13 @@ public class ReaderFunctionService {
     @Autowired
     private UserBkunitDAO userBkunitDAO;
 
-    public  Page<UserBkunit> queryborrowedBooks(int start, int size)
-    {
+    @Autowired
+    private UserFavoriteBookDAO userFavoriteBookDAO;
+
+    @Autowired
+    private BookDAO bookDAO;
+
+    public Page<UserBkunit> queryborrowedBooks(int start, int size) {
         User reader = userService.getUser();
         start = start < 0 ? 0 : start;
         Sort sort = new Sort(Sort.Direction.DESC, "date");
@@ -35,5 +44,38 @@ public class ReaderFunctionService {
         Page<UserBkunit> page = userBkunitDAO.findAllByUser(reader, pageable);
         return page;
     }
+
+    public Page<UserFavoriteBook> queryFavoriteBooks(int start, int size) {
+        User reader = userService.getUser();
+        start = start < 0 ? 0 : start;
+        Sort sort = new Sort(Sort.Direction.DESC, "date");
+        Pageable pageable = PageRequest.of(start, size, sort);
+        Page<UserFavoriteBook> page = userFavoriteBookDAO.findAllByUser(reader, pageable);
+        return page;
+    }
+
+
+    public boolean addFavoriteBook(Book book) {
+        try {
+            User reader = userService.getUser();
+            UserFavoriteBook fb = userFavoriteBookDAO.findByUserandAndBook(reader,book);
+            if (fb == null) userFavoriteBookDAO.save(fb);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteFavoriteBook(Book book) {
+        try {
+            User reader = userService.getUser();
+            UserFavoriteBook fb = userFavoriteBookDAO.findByUserandAndBook(reader,book);
+            if (fb != null) userFavoriteBookDAO.delete(fb);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
