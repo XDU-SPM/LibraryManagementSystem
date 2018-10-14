5aa717593e9d2f5a76931251ddcbc0dc23392ed7
Captain <onepiece.captain97@gmail.com>
2018-10-14 22:47:06 +0800

    init

--- src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
@@ -15,7 +15,8 @@ public class LibraryManagementSystemApplication
     public LocaleResolver localeResolver()
     {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.CHINA);
+//        slr.setDefaultLocale(Locale.CHINA);
+        slr.setDefaultLocale(Locale.US);
         return slr;
     }
 
