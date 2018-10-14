c490a8e10de72975485ea5eb9b1a3930d4a11996
chunjieshenwang <453720489@qq.com>
2018-10-14 23:08:56 +0800

    librarian

--- src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/library_management_system/LibraryManagementSystemApplication.java
@@ -15,7 +15,7 @@ public class LibraryManagementSystemApplication
     public LocaleResolver localeResolver()
     {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.CHINA);
+        slr.setDefaultLocale(Locale.US);
         return slr;
     }
 
