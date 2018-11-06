23b44a76328df6fb652a546a6245cf4a43a82c4a
dell-pc <565267339@qq.com>
2018-11-06 11:29:49 +0800

    init

--- src/main/java/com/example/library_management_system/controller/BookController.java
+++ src/main/java/com/example/library_management_system/controller/BookController.java
@@ -4,16 +4,18 @@ import com.example.library_management_system.bean.Book;
 import com.example.library_management_system.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class BookController
 {
     @Autowired
     private BookService bookService;
 
     @RequestMapping(value = {"/searchBook", "/reader/searchBook"}, method = RequestMethod.GET)
+    @ResponseBody
     public Page<Book> searchBook(String string,
                                  @RequestParam(value = "start", defaultValue = "0") int start,
                                  @RequestParam(value = "size", defaultValue = "10") int size)
@@ -22,6 +24,7 @@ public class BookController
     }
 
     @RequestMapping(path = {"/reader/showbook", "/showbook"}, method = RequestMethod.GET)
+    @ResponseBody
     public Page<Book> showBook(@RequestParam(value = "start", defaultValue = "0") int start,
                                @RequestParam(value = "size", defaultValue = "10") int size, String category)
     {
--- src/main/java/com/example/library_management_system/service/CustomUserService.java
+++ src/main/java/com/example/library_management_system/service/CustomUserService.java
@@ -3,6 +3,7 @@ package com.example.library_management_system.service;
 import com.example.library_management_system.bean.Role;
 import com.example.library_management_system.bean.User;
 import com.example.library_management_system.dao.UserDAO;
+import com.example.library_management_system.utils.RoleUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -24,11 +25,24 @@ public class CustomUserService implements UserDetailsService
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException
     {
-        User user = userDAO.findByUsername(s);
+        String[] strings = s.split(" ");
+        String type = "0";
+        if (strings.length == 2)
+            type = strings[1];
+        User user = userDAO.findByUsername(strings[0]);
+        if (user == null)
+            throw new UsernameNotFoundException("233");
         List<GrantedAuthority> authorities = new ArrayList<>();
         Set<Role> roleSet = user.getRoles();
-        for (Role role : roleSet)
-            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        if ("0".equals(type)
+                || ("1".equals(type) && (RoleUtil.roleContainsString(roleSet, RoleUtil.ROLE_READER_CHECK) || RoleUtil.roleContainsString(roleSet, RoleUtil.ROLE_LIBRARIAN_CHECK)))
+                || "2".equals(type) && RoleUtil.roleContainsString(roleSet, RoleUtil.ROLE_ADMIN))
+        {
+            for (Role role : roleSet)
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        else
+            throw new UsernameNotFoundException("233");
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
     }
 }
